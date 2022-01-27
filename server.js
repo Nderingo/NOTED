0d7f0ca36806af661cffcae221c19a340f90b7cd
Emmanuel Lyimo <esal29t@gmail.com>
2022-01-27 01:08:03 -0800

    modified server.js and routes folder

--- server.js
+++ server.js
@@ -1,5 +1,31 @@
+const express = require('express');
+const path = require('path');
+const routes = require('./routes');
 
+const PORT = process.env.port || 3001;
+const app = express();
 
+app.use(express.static('public'));
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-const PORT = process.env.PORT || 3001;
+// GET Route for homepage
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+// GET Route for feedback page
+app.get('/noted', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/pages/notes.html'))
+);
+
+// Wildcard route to direct users to a 404 page
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, 'public/pages/404.html'))
+);
+
+// Listen from port 3001
+app.listen(PORT, () =>
+  console.log(`Example App listening at http://localhost:${PORT} 🚀`)
+);
